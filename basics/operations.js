74bb3652457979ebb951d6030cc572aa7cbaec45
Vishalbokare45 <vishalbokare45@gmail.com>
2025-02-14 10:58:55 +0000

    done

---
+++ basics/operations.js
@@ -0,0 +1,49 @@
+
+let a=31;
+let b=19;
+
+console.log(a+b)
+
+
+console.log(a++)
+a++;
+console.log(a)
+
+
+console.log(b++)
+b++;
+console.log(b)
+
+
+
+
+
+
+
+
+let st1="Vishal"
+let st2="Bokare"
+
+console.log(st1 + st2)
+
+
+
+let str1="Vishal"
+
+let num=4005;
+
+console.log(str1+" "+num)
+
+
+
+let c=20;
+
+let i=1;
+
+while(i<=5){
+
+   console.log( c/=2);  assignment operators +=,-=,*= anyone of this can be used.
+    i+=1;
+   
+}
+
