92f358ab55848213ab12b33f75636ed3f9c96ffa
Vishalbokare45 <vishalbokare45@gmail.com>
2025-02-14 07:29:53 +0000

    datatpes save it

---
+++ basics/datatypes.js
@@ -0,0 +1,22 @@
+
+let name="Vishal"
+let age=21;
+let village="someshwar"
+
+let isMarried=false;
+let rollNo;
+
+console.table([name,age,village,isMarried])
+
+console.log(typeof(age))
+console.log(typeof(name))
+
+
+console.log(typeof(isMarried))
+
+console.log(rollNo)  // it gives undefined because it not intialized.   undefined is tye of itself.
+
+
+rollNo=null;  // the type of null is object..
+console.log(rollNo)  
+
