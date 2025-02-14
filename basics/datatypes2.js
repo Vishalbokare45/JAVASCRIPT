1848b3d1c8a7c13733d996588ac60271426c0e09
Vishalbokare45 <vishalbokare45@gmail.com>
2025-02-14 09:29:07 +0000

    done

---
+++ basics/datatypes2.js
@@ -0,0 +1,21 @@
+
+
+let myName="Vishal"
+
+console.log(myName)
+
+console.log(typeof(myName)) // type is string
+
+let covert=Number(myName)  // it covert to number but its value becomes Not a Number(NAN)
+
+console.log(covert)  // values becomes NAN
+console.log(typeof(covert)) // type into number
+
+
+let sum= 21;
+console.log(typeof(sum));
+
+let covertToString= String(sum)
+
+console.log(covertToString)
+console.log()
