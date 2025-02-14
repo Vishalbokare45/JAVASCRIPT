41d65aa822e60f36b650e0aa1e5f103f894662ef
Vishalbokare45 <vishalbokare45@gmail.com>
2025-02-14 09:58:34 +0000

    work

---
+++ basics/conversions.js
@@ -0,0 +1,38 @@
+
+let sum=21;
+
+console.log(sum)
+
+console.log(typeof(sum))
+
+let convertToString=String (sum)  // here number is convert to string 
+
+console.log(convertToString)
+console.log(typeof(convertToString))  
+
+
+let isValid=true
+console.log(isValid)
+
+let ToString= String(isValid)  
+
+console.log(ToString)
+console.log(typeof(ToString))
+
+
+let num=0
+
+let isCheck= Boolean(num)
+
+console.log(isCheck)
+console.log(typeof(isCheck))
+
+
+let char= 'v'
+
+console.log(char)
+
+console.log(typeof(char))
+
+
+
