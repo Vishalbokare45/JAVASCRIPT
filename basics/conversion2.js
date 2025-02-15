d7f5c56481634f49dc1ddbbab27cc1cb6e737e8f
Vishalbokare45 <vishalbokare45@gmail.com>
2025-02-15 06:16:05 +0000

    save

---
+++ basics/conversion2.js
@@ -0,0 +1,27 @@
+
+
+// console.log(2>1)
+// console.log(2<1)
+// console.log(2==1)  // this are same dataypes conversions and this are more frequently use.
+
+// console.log(100>  "222")
+// console.log("2">1)
+// console.log("22">1) // here, String converted to number then it compare to number.
+
+
+console.log("vishal">="bokare")
+console.log("vishal"<"yzx")  // here in both string it comapre only the first letter 
+
+// console.log(null>0)
+// console.log(null>=0)  
+// console.log(null==0) 
+
+// console.log(undefined >=0)
+// console.log(undefined==0)  // Here, all gives false value because undefined doesn't have any value to compare
+// console.log(undefined>0)
+
+
+// from here we conculed that, we have to use only conversion of same datatypes because different dataypes output are inconsistance or we can't predict.
+
+
+
