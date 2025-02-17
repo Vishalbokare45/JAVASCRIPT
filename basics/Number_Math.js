f69fb3c9e8c8a627eac6969dbf965c9f807e5fdc
Vishalbokare45 <vishalbokare45@gmail.com>
2025-02-17 12:17:56 +0000

    save

---
+++ basics/Number_Math.js
@@ -0,0 +1,43 @@
+
+let score=400
+
+console.log(score)
+
+const num=new Number(1000)
+console.log(num)
+
+const num1=99.8977996
+
+console.log(num1.toFixed(3))
+console.log(num1.toFixed(1))
+
+console.log(num1.toPrecision(2))
+
+console.log(num1.toString())
+console.log(typeof(num1))
+ console.log(num1.toString().length)
+
+
+ const num2=10000000
+ console.log(num2.toLocaleString('en-IN'))
+
+// +++++++++++++++++ Math++++++++++++++++
+
+
+console.log(Math.PI)
+
+console.log(Math.abs(-1045))
+console.log(Math.sqrt(25))
+
+console.log(Math.round(1.3456))
+console.log(Math.round(1.786))
+
+
+console.log(Math.ceil(20.12345))
+console.log(Math.ceil(20.8766))
+console.log(Math.floor(20.12345))
+console.log(Math.floor(20.8766))
+
+
+console.log((Math.random()*10)+1)
+console.log(Math.random())
---
+++ basics/dates.js
@@ -0,0 +1,20 @@
+
+
+// let myDate=new Date()
+
+// console.log(myDate)
+
+// console.log(myDate.toDateString())
+// console.log(myDate.toISOString())
+
+// console.log(myDate.toLocaleDateString())
+
+
+let myCreatedDate= new Date(2024, 2,25)
+
+console.log(myCreatedDate.toDateString())
+console.log(myCreatedDate.toLocaleString())
+
+let newDates=new Date("2025/10/31")
+
+console.log(newDates.toLocaleDateString())
