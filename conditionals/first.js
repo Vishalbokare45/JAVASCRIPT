4e34885dd5b0d42e6d7adea8dac0af130c9d8fb0
Vishalbokare45 <vishalbokare45@gmail.com>
2025-02-20 11:33:28 +0000

    save

---
+++ conditionals/first.js
@@ -0,0 +1,82 @@
+
+ 
+
+if(true){
+
+    console.log("Heyy Guys !!");
+
+    console.log("My Name is \'Visha Bokare\'")
+}
+
+
+
+//  # if -else
+
+let isValid=true;
+
+if(!isValid){
+
+    console.log("Given name is valid");
+}
+else{
+
+    console.log("Given name is not valid")
+}
+
+
+
+// if-else if else
+
+
+num1= 11;
+
+
+if(num1 %2==0){
+
+    console.log(` given num ${num1}  is even`)
+}
+else if(num1%3 ==0){
+
+    console.log(` given num ${num1}  is  multiply of 3`)
+
+
+}
+else {
+
+    console.log("Given num is not even or odd.")
+}
+
+
+const balance=3000;
+
+
+if(balance>2000){
+
+    let  isParty=true;
+
+    console.log(`${balance} is more than 2000 and party ${isParty}`)
+}
+
+
+let month=4;
+
+switch(month){
+
+    case 1:
+        console.log("January");
+        break;
+    
+    case 2:
+            console.log("Feb");
+            break;    
+    case 3:
+                console.log("March");
+                break;
+    case 4:
+                    console.log("April");
+                    break;
+
+    default:
+        console.log("default match")
+
+}
