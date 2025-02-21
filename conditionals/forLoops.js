9bbacbf9aa13f1d6af0f56e14c75a93f94e0b3ce
Vishalbokare45 <vishalbokare45@gmail.com>
2025-02-21 09:32:36 +0000

    Save

---
+++ conditionals/forLoops.js
@@ -0,0 +1,74 @@
+
+
+
+// for of
+
+
+// let arr=[1,2,3,"vishal","VIT"];
+
+
+// for (const ele of arr) {
+
+//     console.log(ele);
+    
+// }
+
+
+// For in loop  objects
+
+// const MyInfo={
+
+//     myName:"Vishal",
+//     age:21,
+//     cllg:"VIT",
+//     city:"Pune"
+// }
+
+// // objects for of not useful 
+
+// for (const key in MyInfo) {
+   
+
+//     console.log(`${key}  bleongs to ${MyInfo[key]}`)
+// }
+
+// // for-in used for objects and array but it gives keys
+
+// let myArr=["IN","UK","USA","LONDON"];
+
+// for (const key in myArr) {
+   
+//     console.log(myArr[key])
+// }
+
+
+//  for-each on arrays
+
+let MyArr2=["java","js","python","ruby","C#"]
+
+
+// MyArr2.forEach(function(item){
+
+
+//     console.log(item);
+
+// })
+
+// 
+
+
+let nums=[1,2,3,4,5,56,7,8,9,120]
+
+nums.forEach((key, index,Array)=>{
+
+    console.log(key,"index:",index,Array)
+});
+
+
+
+
+
+
+
+
+
